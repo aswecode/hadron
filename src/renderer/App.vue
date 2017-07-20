e6423c6ccd295d29feb7375804c18f369df6810d
Antonio Gil <antonioluisgil@gmail.com>
2017-07-20 20:03:04 +0200

    Created initial project structure with electron-vue template

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hadron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
