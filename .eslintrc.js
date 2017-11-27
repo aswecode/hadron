234afa1916221e06fd7032017ac80be4cf377434
Antonio Gil <antonioluisgil@gmail.com>
2017-11-27 20:12:11 +0100

    Updated libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,11 +22,11 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 1,
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'no-unused-vars': ['warn'],
     'no-console': 0
   }
 }
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app" class="app">
+	<div class="app">
 		<div class="app-expander">
 			<!-- START: Toolbar component -->
 			<div class="toolbar-tray">
